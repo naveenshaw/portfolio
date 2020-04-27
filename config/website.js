8d13c03640dbbff8d73e3da3409944b888a5b111
naveenshaw <64439964+naveenshaw@users.noreply.github.com>
2020-04-27 16:37:32 -0500

    Add files via upload

---
+++ config/website.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+  siteTitle: 'Naveen Shaw', // Navigation and Site Title
+  siteTitleAlt: 'Naveen Shaw', // Alternative Site title for SEO
+  siteTitleShort: 'naveenshaw', // short_name for manifest
+  siteHeadline: '', // Headline for schema.org JSONLD
+  siteUrl: 'https://www.naveen.github.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteBanner: '/banner.png', // Used for Social Preview
+  siteDescription: 'Research and Development Portfolio of Naveen Shaw',
+  author: 'Naveen Shaw', // Author for schema.org JSONLD
+  icon: 'src/images/logo.png',
+
+  siteFBAppID: 'naveenshaw1998@facebook.com', // Facebook App ID - Optional
+  userTwitter: '@naveenshaw', // Twitter Username
+
+  // Manifest and Progress color
+  themeColor: '#69dd04',
+  backgroundColor: '#161619'
+};
